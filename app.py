467aac1b1aa44e699b489b1b3576cf63e6492984
Norman <norman.coloma@voicemode.net>
2023-12-18 20:50:28 +0100

    Removing code

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import logging
-from functools import wraps
-
 from flask import Flask
 from infraestructure.rest.error_handler import handle_exception
 from infraestructure.rest.team_controller import team_controller
@@ -11,19 +9,6 @@ app.register_error_handler(Exception, handle_exception)
 
 logging.basicConfig(level=logging.INFO, format='{"dateTime": "%(asctime)s", "level": "info", "message": "%(message)s"}',
                     datefmt='%Y-%m-%d %H:%M:%S')
-
-
-def has_permissions(permission):
-    def decorated_function(f):
-        @wraps(f)
-        def wrapped(*args, **kwargs):
-            if permission == 'read':
-                print('has permissions')
-                return f(*args, **kwargs)
-
-        return wrapped
-
-    return decorated_function
 
 
 if __name__ == '__main__':
